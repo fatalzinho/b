7c65c845f660fdef958c82948e8c129065880ef2
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2019-11-09 21:05:41 -0300

    update 2

--- instal.sh
+++ instal.sh
@@ -10,23 +10,10 @@ function espera() {
 	read -p "$1 Tecle <ENTER> para continuar..." a;
 	unset a;
 }
-url="https://www.archlinux.org/mirrorlist/?country=BR&use_mirror_status=on"
-
-  tmpfile=$(mktemp --suffix=-mirrorlist)
-
-  # Get latest mirror list and save to tmpfile
-  curl -so ${tmpfile} ${url}
-  sed -i 's/^#Server/Server/g' ${tmpfile}
-
-  # Backup and replace current mirrorlist file (if new file is non-zero)
-  if [[ -s ${tmpfile} ]]; then
-   { echo " Backing up the original mirrorlist..."
-     mv -i /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig; } &&
-   { echo " Rotating the new list into place..."
-     mv -i ${tmpfile} /etc/pacman.d/mirrorlist; }
-  else
-    echo " Unable to update, could not download list."
-  fi
+echo "NOME ROOT"
+read nome
+echo "SENHA ROOT"
+read root_senha
 
 #FORMATAR HD
 
@@ -51,9 +38,51 @@ sleep 1
 sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//;n;s/^#//}' /etc/pacman.conf 
 sleep 1
 
-pacstrap /mnt base base-devel linux linux-firmware nano sudo man-db
+pacstrap /mnt base base-devel linux linux-firmware nano sudo man-db dhcpcd git usbutils diffutils
 espera
 
 genfstab -U -p /mnt >> /mnt/etc/fstab 
 sleep 1
+
+arch-chroot /mnt << EOF
+
+# Sets hostname
+echo $nome > /etc/hostname
+cp /etc/hosts /etc/hosts.bkp
+sed 's/localhost$/localhost '$nome'/' /etc/hosts > /tmp/hosts
+mv /tmp/hosts /etc/hosts
+
+sed -i 's/#\('pt_BR.UTF-8'\)/\1/' /etc/locale.gen
+sed -i 's/#\('pt_BR'\)/\1/' /etc/locale.gen
+
+systemctl enable dhcpcd.service
+
+echo -e "KEYMAP=br-abnt2\nFONT=lat0-16\nFONT_MAP=" >> /etc/vconsole.conf
+sleep 1
+echo  "LANG=pt_BR.UTF-8" >> /etc/locale.conf
+loadkeys /usr/share/kbd/keymaps/i386/qwerty/br-abnt2.map.gz
+
+locale-gen
+
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+echo America/Sao_Paulo > /etc/timezone
+hwclock --systohc --utc
+
+mkinitcpio -p linux
+pacman -S grub-bios
+grub-install --target=i386-pc --recheck --debug /dev/sda
+mkdir -p /boot/grub/locale
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+pacman -S os-prober 
+os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo -e $root_senha"\n"$root_senha | passwd
+EOF
+sleep 1
 arch-chroot /mnt
+
+
+SLEEP 5
+umount /mnt/{boot,home,}
+reboot
