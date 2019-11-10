51791662105c834d8b2eab6bc78459fb0033e120
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2019-11-10 11:38:23 -0300

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -10,10 +10,16 @@ function espera() {
 	read -p "$1 Tecle <ENTER> para continuar..." a;
 	unset a;
 }
+#nome root
 echo "NOME ROOT"
 read nome
 echo "SENHA ROOT"
 read root_senha
+#nome usuario
+echo "NOME USUARIO"
+read usuario
+echo "SENHA USUARIO"
+read usr_usuario
 
 #FORMATAR HD
 
@@ -21,36 +27,57 @@ read root_senha
 cfdisk 
 
 mkfs.ext4 $boot -L Boot 1> /dev/null 2> /dev/stdout
-sleep 2
+sleep 7
+echo "formatando BOOT"
+sleep 7
+clear 
 mkfs.ext4 $raiz -L Raiz 1> /dev/null 2> /dev/stdout
-sleep 2
+sleep 7
+echo "formatando RAIZ"
+sleep 7
+clear 
 mkfs.ext4 $home -L Home  1> /dev/null 2> /dev/stdout
-sleep 2
+sleep 7
+echo "formatando HOME"
+sleep 7
+clear 
 mkswap -L swap $swap && swapon $swap
-sleep 2
+sleep 7
+echo "montando SWAP"
+sleep 7
+clear 
 mount $raiz /mnt 
-sleep 1
+sleep 7
+echo "montando MNT"
+sleep 7
+clear 
 mkdir /mnt/boot && mount $boot /mnt/boot
-sleep 1
+sleep 7
+echo "montando BOOT"
+sleep 7
+clear 
 mkdir /mnt/home && mount $home /mnt/home 
-sleep 1
-
+sleep 7
+echo "montando HOME"
+sleep 7
+clear 
 sed -i '/^#\[multilib\]/{s/^#//;n;s/^#//;n;s/^#//}' /etc/pacman.conf 
-sleep 1
-
+sleep 5
+clear 
 pacstrap /mnt base base-devel linux linux-firmware nano sudo man-db dhcpcd git usbutils diffutils
-espera
-
+sleep 7
+echo "instalando pacstrap"
+sleep 7
+clear 
 genfstab -U -p /mnt >> /mnt/etc/fstab 
-sleep 1
-
+sleep 30
+echo "entrando chroot"
+sleep 30
+clear 
 arch-chroot /mnt << EOF
 
 # Sets hostname
 echo $nome > /etc/hostname
-cp /etc/hosts /etc/hosts.bkp
-sed 's/localhost$/localhost '$nome'/' /etc/hosts > /tmp/hosts
-mv /tmp/hosts /etc/hosts
 
 sed -i 's/#\('pt_BR.UTF-8'\)/\1/' /etc/locale.gen
 sed -i 's/#\('pt_BR'\)/\1/' /etc/locale.gen
@@ -73,11 +100,23 @@ pacman -S grub-bios
 grub-install --target=i386-pc --recheck --debug /dev/sda
 mkdir -p /boot/grub/locale
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+sudo pacman -S gvfs-mtp ntfs-3g autofs mtpfs firefox thunar unzip unrar alsa-utils alsa-plugins --noconfirm
+
 pacman -S os-prober 
 os-prober
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo -e $root_senha"\n"$root_senha | passwd
+#echo -e $root_senha"\n"$root_senha | passwd
+passwd << EOF
+$root_senha
+$root_senha
+EOF
+
+useradd -m -G audio,dbus,lp,network,optical,power,storage,users,video,wheel -s /bin/bash $nome;
+passwd "$usuario" << EOF
+$usr_usuario
+$usr_usuario
+EOF
 EOF
 sleep 1
 arch-chroot /mnt
