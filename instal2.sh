91f349debb41cb49c82e60f9ed68ff13c7d90146
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2019-11-16 15:05:09 -0300

    Hu

--- instal2.sh
+++ instal2.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+sudo git clone https://aur.archlinux.org/nvidia-340xx.git
+
+sudo chmod +777 /nvidia-340xx/
+cd /nvidia-340xx/
+makepkg -si
+
+sudo pacman -S lxde lxdm
+systemctl enable lxdm.service
 
