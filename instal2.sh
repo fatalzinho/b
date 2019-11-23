e5e0c94f654426edfd5dcd57944ddc2d739bd531
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2019-11-23 12:07:11 -0300

    Update instal2.sh

--- instal2.sh
+++ instal2.sh
@@ -4,10 +4,10 @@ function espera() {
 	unset a;
 
 #sudo pacman -S intel-ucode
-sudo pacman -S nvidia-390xx nvidia-390xx-libgl nvidia-390xx-utils lib32-nvidia-390xx-libgl 
-sudo pacman -S  alsa-util salsa-lib alsa-plugins alsa-firmware pulseaudio paprefs pavucontrol 
-sudo pacman -S pulseaudio-alsa lib32-libpulse lib32-alsa-plugins
-sudo rmmod snd_pcm_oss
+pacman -S nvidia-390xx nvidia-390xx-libgl nvidia-390xx-utils lib32-nvidia-390xx-libgl 
+pacman -S  alsa-util salsa-lib alsa-plugins alsa-firmware pulseaudio paprefs pavucontrol 
+pacman -S pulseaudio-alsa lib32-libpulse lib32-alsa-plugins
+rmmod snd_pcm_oss
 #lib32-nvidia-libgl 
 #lib32-nvidia-utils
 #nvidia
@@ -17,9 +17,11 @@ sudo rmmod snd_pcm_oss
 #opencl-nvidia
 lspci -k | grep -A 2 -E "(VGA|3D)"
 espera
-sudo pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter networkmanager network-manager-applet
-#sudo pacman -S lxde lxdm --noconfirm
-#sudo systemctl enable lxdm.service
-#sudo echo  "exec startlxde" >> ~/.xinitrc
-sudo pacman -S xdg-user-dirs --noconfirm
-sudo xdg-user-dirs-update
+pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter networkmanager network-manager-applet --noconfirm
+systemctl enable lightdm
+systemctl enable NetworkManager
+#pacman -S lxde lxdm --noconfirm
+#systemctl enable lxdm.service
+#echo  "exec startlxde" >> ~/.xinitrc
+pacman -S xdg-user-dirs --noconfirm
+xdg-user-dirs-update
