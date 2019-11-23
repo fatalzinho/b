46114101c2600446e257fa58c21907213a604c1b
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2019-11-22 21:17:19 -0300

    Update instal2.sh

--- instal2.sh
+++ instal2.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-sudo git clone https://aur.archlinux.org/nvidia-340xx.git
-
-sudo chmod +777 /nvidia-340xx/
-cd /nvidia-340xx/
-makepkg -si
-
-sudo pacman -S lxde lxdm
+#sudo pacman -S intel-ucode
+sudo pacman -S nvidia-390xx nvidia-390xx-libgl nvidia-390xx-utils lib32-nvidia-390xx-libgl 
+sudo pacman -S  alsa-util salsa-lib alsa-plugins alsa-firmware pulseaudio paprefs pavucontrol 
+sudo pacman -S pulseaudio-alsa lib32-libpulse lib32-alsa-plugins
+sudo rmmod snd_pcm_oss
+#lib32-nvidia-libgl 
+#lib32-nvidia-utils
+#nvidia
+#nvidia-ck-k10
+#nvidia-libgl
+#nvidia-utils
+#opencl-nvidia
+lspci -k | grep -A 2 -E "(VGA|3D)"
+sudo pacman -S lxde lxdm --noconfirm
 sudo systemctl enable lxdm.service
-
 sudo echo  "exec startlxde" >> ~/.xinitrc
+sudo pacman -S xdg-user-dirs --noconfirm
+sudo xdg-user-dirs-update
+pacman -S network-manager-applet --noconfirm
+sudo hwclock — systohc — utc
