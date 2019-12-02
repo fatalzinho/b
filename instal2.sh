d6c408feaca1d217fae924ea12bd13e7bbfde155
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2019-12-02 20:50:56 -0300

    att

--- instal2.sh
+++ instal2.sh
@@ -2,23 +2,14 @@
 
 #sudo pacman -S intel-ucode
 sudo pacman -S nvidia-390xx nvidia-390xx-libgl nvidia-390xx-utils lib32-nvidia-390xx-libgl --noconfirm
-sudo pacman -S  alsa-util salsa-lib alsa-plugins alsa-firmware pulseaudio paprefs pavucontrol --noconfirm
+sudo pacman -S opencl-nvidiapul nvidia-settings seaudio paprefs pavucontrol --noconfirm
 sudo pacman -S pulseaudio-alsa lib32-libpulse lib32-alsa-plugins --noconfirm
 sudo rmmod snd_pcm_oss
-#lib32-nvidia-libgl 
-#lib32-nvidia-utils
-#nvidia
-#nvidia-ck-k10
-#nvidia-libgl
-#nvidia-utils
-#opencl-nvidia
-lspci -k | grep -A 2 -E "(VGA|3D)"
-sudo pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter networkmanager network-manager-applet --noconfirm
-sudo systemctl enable lightdm
+#lspci -k | grep -A 2 -E "(VGA|3D)"
+sudo pacman -S xfce4 xfce4-goodies lxdm networkmanager network-manager-applet --noconfirm
+sudo systemctl enable lxdm.service
 sudo systemctl enable NetworkManager
-#pacman -S lxde lxdm --noconfirm
-#systemctl enable lxdm.service
-#echo  "exec startlxde" >> ~/.xinitrc
+sudo echo  "exec startxfce4" >> ~/.xinitrc
 sudo pacman -S xdg-user-dirs --noconfirm
 sudo xdg-user-dirs-update
 sudo pacman -S gnome-software gnome-software-packagekit-plugin lightdm-gtk-greeter-settings --noconfirm
@@ -27,7 +18,7 @@ sudo pacman -S thunar gtk-theme-switch2 mate-icon-theme materia-gtk-theme gnome-
 sudo pacman -S mate-icon-theme-faenza mate-themes arc-gtk-theme xfce4-taskbar-plugin --noconfirm
 sudo pacman -S ttf-dejavu ttf-liberation noto-fonts --noconfirm
 fc-cache -vf
-
+sudo timedatectl set-ntp true
 sudo git clone https://aur.archlinux.org/mugshot.git
 sudo chmod 777 mugshot/
 cd mugshot
