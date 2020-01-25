2a35e34bc15fcf009a488d825a459387d7ea2f35
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2020-01-25 13:14:47 -0300

    up2021

--- xfce4.sh
+++ xfce4.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 sudo pacman -Syy
+sudo timedatectl set-ntp true
+
+# Instalação driver de VIDEO e SOM
 pkg="linux-zen-headers 
 nvidia-390xx-dkms nvidia-390xx-libgl nvidia-390xx-utils lib32-nvidia-390xx-libgl 
 opencl-nvidia nvidia-settings 
 seaudio paprefs pavucontrol pulseaudio-alsa lib32-libpulse lib32-alsa-plugins 
 pulseaudio-alsa lib32-libpulse lib32-alsa-plugins" 
- 
-
 sudo pacman -S $pkg --noconfirm
+sudo rmmod snd_pcm_oss
 
+# Instalação DESKTOP e APLICATIVOS
 pkgxfce4="xfce4 
 xfce4-goodies
 xdg-user-dirs 
@@ -21,33 +24,37 @@ ttf-dejavu ttf-liberation noto-fonts
 xfce4-taskbar-plugin 
 p7zip unrar tar rsync 
 file-roller 
+playonlinux 
+telegram-desktop 
 neofetch"
-
 sudo pacman -S $pkgxfce4 --noconfirm
+sudo echo  "exec startxfce" >> ~/.xinitrc
+sudo systemctl enable lxdm.service
+sudo xdg-user-dirs-update
 
+# Instalação de TEMAS
 pkdtheme="gtk-theme-switch2 
 mate-icon-theme 
 materia-gtk-theme 
-gnome-icon-theme-extras
 mate-icon-theme-faenza 
 mate-themes 
 arc-gtk-theme" 
-
-sudo echo  "exec startxfce" >> ~/.xinitrc
-
 sudo pacman -S $pkdtheme --noconfirm
 
-sudo rmmod snd_pcm_oss
-
-sudo systemctl enable lxdm.service
-
-sudo xdg-user-dirs-update
- 
-#fc-cache -vf
-sudo timedatectl set-ntp true
-
-cd /tmp/
+# OUTROS
+cd /Documentos/
 git clone https://aur.archlinux.org/mugshot.git
 cd mugshot
 makepkg -si --noconfirm
+
+cd /Documentos/
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
 reboot
+
+# Depois quem entra no desktop mudar janela de XFWM4 para OPENBOX
+#killall openbox
+#xfwm4 --replace &
+
