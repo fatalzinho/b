08d61101bf57aaf81f022ccaf194d71627274863
fatalzinho <47801679+fatalzinho@users.noreply.github.com>
2020-02-03 23:00:35 -0300

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -3,8 +3,7 @@ sudo pacman -Syy
 sudo timedatectl set-ntp true
 
 # Instalação driver de VIDEO e SOM
-pkg="linux-zen-headers 
-nvidia-390xx-dkms nvidia-390xx-libgl nvidia-390xx-utils lib32-nvidia-390xx-libgl 
+pkg="nvidia-390xx-dkms nvidia-390xx-libgl nvidia-390xx-utils lib32-nvidia-390xx-libgl 
 opencl-nvidia nvidia-settings 
 pulseaudio paprefs pavucontrol pulseaudio-alsa lib32-libpulse lib32-alsa-plugins 
 pulseaudio-alsa lib32-libpulse lib32-alsa-plugins" 
@@ -19,10 +18,12 @@ thunar
 gparted 
 lxdm 
 sddm 
+gedit 
+gnome-disk-utility 
 firefox 
 firefox-i18n-pt-br 
 ttf-dejavu ttf-liberation noto-fonts 
-p7zip unrar tar rsync 
+p7zip unrar unzip tar rsync 
 file-roller 
 playonlinux 
 telegram-desktop 
@@ -43,12 +44,12 @@ arc-gtk-theme"
 sudo pacman -S $pkdtheme --noconfirm
 
 # OUTROS
-cd /Documentos/
+cd /tmp/
 git clone https://aur.archlinux.org/mugshot.git
 cd mugshot
 makepkg -si --noconfirm
 
-cd /Documentos/
+cd /tmp/
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
